4b005f9fa3de18299ed26317d56aea16fc35e6d7
cristiansanchez2715 <cristiansanchez2715@gmail.com>
2024-06-04 14:15:49 -0500

    actualizando backend

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const corsOptions = {
 
   app.use(cors(corsOptions));
   app.use(express.json());
+  app.use(express.urlencoded({ extended: true })); 
 
 app.listen(port, () => {
     console.log("base de datos jlproduccions conected")
